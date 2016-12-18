0bdd27d7238e4e5e2c545a68c9ae9cb4833e466a
Luke Shiner <luke@lukeshiner.com>
2016-12-18 15:18:20 +0000

    Fixes MEDIA_ROOT and STATIC_ROOT in settings.py

--- beutiful/settings.py
+++ beutiful/settings.py
@@ -108,12 +108,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(
-    os.path.join(os.path.dirname(BASE_DIR), '/public/static'))
+STATIC_ROOT = os.path.join(
+    os.path.abspath(os.path.dirname(BASE_DIR)), 'public', 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.abspath(
-    os.path.dirname(BASE_DIR) + '/public/media')
+MEDIA_ROOT = os.path.join(os.path.abspath(
+    os.path.dirname(BASE_DIR)), 'public', 'media')
 
 
 try:
