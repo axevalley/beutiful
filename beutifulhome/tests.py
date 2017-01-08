f9ceee8477447478dec64d3d92b0313ba7c9badb
Luke Shiner <luke@lukeshiner.com>
2017-01-08 16:12:45 +0000

    Add tests to beutifulhome

--- beutifulhome/tests.py
+++ beutifulhome/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_uses_index_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'beutifulhome/index.html')
+
+    def test_uses_about_template(self):
+        response = self.client.get('/about/')
+        self.assertTemplateUsed(response, 'beutifulhome/about.html')
+
+    def test_uses_contact_template(self):
+        response = self.client.get('/contact/')
+        self.assertTemplateUsed(response, 'beutifulhome/contact.html')
+
+    def test_uses_friends_template(self):
+        response = self.client.get('/friends/')
+        self.assertTemplateUsed(response, 'beutifulhome/friends.html')
+
+    def test_uses_products_template(self):
+        response = self.client.get('/products/')
+        self.assertTemplateUsed(response, 'beutifulhome/products.html')
+
+    def test_uses_services_template(self):
+        response = self.client.get('/services/')
+        self.assertTemplateUsed(response, 'beutifulhome/services.html')
