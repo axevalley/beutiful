985cdce31b30ccec99078e23bc221e1cdc4fbccd
Luke Shiner <luke@lukeshiner.com>
2017-01-13 18:59:46 +0000

    Update repo url in fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 'ssh://lseagle@lsgit.uk/~/lsgit.uk/beutiful.git'
+REPO_URL = 'https://github.com/axevalley/beutiful.git'
 
 
 def deploy():
