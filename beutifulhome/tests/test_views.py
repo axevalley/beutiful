473bebaf2cc12fde2c43a1e35742cb26a9a1611a
Luke Shiner <luke@lukeshiner.com>
2017-01-27 14:32:11 +0000

    Moves beutifulhome tests into tests folder

--- beutifulhome/tests/test_views.py
+++ beutifulhome/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from beutifulhome.forms import ContactForm
 
 
 class HomePageTest(TestCase):
@@ -7,21 +8,36 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'beutifulhome/index.html')
 
+
+class AboutPageTestTest(TestCase):
+
     def test_uses_about_template(self):
         response = self.client.get('/about/')
         self.assertTemplateUsed(response, 'beutifulhome/about.html')
+
+
+class ContactPageTest(TestCase):
 
     def test_uses_contact_template(self):
         response = self.client.get('/contact/')
         self.assertTemplateUsed(response, 'beutifulhome/contact.html')
 
+
+class FriendsPageTest(TestCase):
+
     def test_uses_friends_template(self):
         response = self.client.get('/friends/')
         self.assertTemplateUsed(response, 'beutifulhome/friends.html')
 
+
+class ProductsPageTest(TestCase):
+
     def test_uses_products_template(self):
         response = self.client.get('/products/')
         self.assertTemplateUsed(response, 'beutifulhome/products.html')
+
+
+class ServicesPageTest(TestCase):
 
     def test_uses_services_template(self):
         response = self.client.get('/services/')
