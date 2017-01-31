c7358c6fd675803b344d2bfafc0a8cb407e33767
Luke Shiner <luke@lukeshiner.com>
2017-01-31 18:42:45 +0000

    Updates form tests for replacement of message type with contact phone

--- beutifulhome/tests/test_forms.py
+++ beutifulhome/tests/test_forms.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from beutifulhome.forms import (
     ContactForm, NO_EMAIL_ADDRESS_MESSAGE, INVALID_EMAIL_ADDRESS_MESSAGE,
-    NO_NAME_MESSAGE, NO_SUBJECT_MESSAGE, NO_TYPE_MESSAGE, NO_CONTENT_MESSAGE)
+    NO_NAME_MESSAGE, NO_SUBJECT_MESSAGE, NO_CONTENT_MESSAGE,
+    INVALID_PHONE_MESSAGE)
 
 
 class ContactFormTest(TestCase):
@@ -18,9 +19,9 @@ class ContactFormTest(TestCase):
         form = ContactForm()
         self.assertIn('placeholder="Message subject"', form.as_p())
 
-    def test_form_renders_message_type_input(self):
+    def test_form_renders_contact_phone_input(self):
         form = ContactForm()
-        self.assertIn('name="message_type"', form.as_p())
+        self.assertIn('name="contact_phone"', form.as_p())
 
     def test_form_renders_message_content_input(self):
         form = ContactForm()
@@ -29,7 +30,7 @@ class ContactFormTest(TestCase):
     def test_form_validation_for_missing_email(self):
         form = ContactForm(data={
             'return_email': '', 'contact_name': 'Philip Jones',
-            'message_subject': 'Hello', 'message_type': 'Query',
+            'message_subject': 'Hello', 'contact_phone': '09879 46587',
             'message_content': 'Hello message'})
         self.assertFalse(form.is_valid())
         self.assertEqual(
@@ -40,7 +41,7 @@ class ContactFormTest(TestCase):
         form = ContactForm(data={
             'return_email': 'invalidemailstring',
             'contact_name': 'Philip Jones', 'message_subject': 'Hello',
-            'message_type': 'Query', 'message_content': 'Hello message'})
+            'contact_phone': '09879 46587', 'message_content': 'Hello message'})
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['return_email'], [INVALID_EMAIL_ADDRESS_MESSAGE])
@@ -48,7 +49,7 @@ class ContactFormTest(TestCase):
     def test_form_validation_for_missing_name(self):
         form = ContactForm(data={
             'return_email': 'noone@example.com', 'contact_name': '',
-            'message_subject': 'Hello', 'message_type': 'Query',
+            'message_subject': 'Hello', 'contact_phone': 'Query',
             'message_content': 'Hello message'})
         self.assertFalse(form.is_valid())
         self.assertEqual(
@@ -58,25 +59,34 @@ class ContactFormTest(TestCase):
         form = ContactForm(data={
             'return_email': 'noone@example.com',
             'contact_name': 'Philip Jones', 'message_subject': '',
-            'message_type': 'Query', 'message_content': 'Hello message'})
+            'contact_phone': '09879 46587',
+            'message_content': 'Hello message'})
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['message_subject'], [NO_SUBJECT_MESSAGE])
 
-    def test_form_validation_for_missing_message_type(self):
+    def test_form_can_be_processed_without_phone_supplied(self):
         form = ContactForm(data={
             'return_email': 'noone@example.com',
             'contact_name': 'Philip Jones', 'message_subject': 'Hello',
-            'message_type': '', 'message_content': 'Hello message'})
+            'contact_phone': '', 'message_content': 'Hello message'})
+        self.assertTrue(form.is_valid())
+
+    def test_only_number_can_be_submitted_in_phone_field(self):
+        form = ContactForm(data={
+            'return_email': 'noone@example.com',
+            'contact_name': 'Philip Jones', 'message_subject': '',
+            'contact_phone': 'Hello Text',
+            'message_content': 'Hello message'})
         self.assertFalse(form.is_valid())
         self.assertEqual(
-            form.errors['message_type'], [NO_TYPE_MESSAGE])
+            form.errors['contact_phone'], [INVALID_PHONE_MESSAGE])
 
     def test_form_validation_for_missing_content(self):
         form = ContactForm(data={
             'return_email': 'noone@example.com',
             'contact_name': 'Philip Jones', 'message_subject': 'Hello',
-            'message_type': 'Query', 'message_content': ''})
+            'contact_phone': '09879 46587', 'message_content': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['message_content'], [NO_CONTENT_MESSAGE])
